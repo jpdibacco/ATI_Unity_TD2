5bfa484149b791914ecaeeff3e8d93c9e22bf128
Patricio <jpdibacco@gmail.com>
2020-11-05 00:37:07 +0100

    added script raquette

---
+++ Assets/Scenes/Raquette.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raquette : MonoBehaviour{
+    public float speed;
+    void Update(){
+        float h = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector3.right * h * speed * Time.deltaTime);
+    }
+}
