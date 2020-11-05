28aa9ca6f173088ae729916491f09bb07a432c93
Patricio <jpdibacco@gmail.com>
2020-11-05 23:29:44 +0100

    Raquette version 2

--- Assets/Scenes/Balle.cs
+++ Assets/Scenes/Balle.cs
@@ -6,6 +6,7 @@ public class Balle : MonoBehaviour{
     public Vector3 deplacement;
     public int nbrBriques;
     public GameObject prefabDebris;
+    public float sidePower = 2f;
     void FixedUpdate(){
         transform.Translate(Time.deltaTime*deplacement);
     }
@@ -14,10 +15,20 @@ public class Balle : MonoBehaviour{
         if(collision.gameObject.tag == "Brique"){
             Destroy(collision.gameObject);
             nbrBriques--;
-            //Instantiate(prefabDebris,collision.transform.position,Quaternion.identity);
+            Instantiate(prefabDebris,collision.transform.position,Quaternion.identity);
         }
         if (nbrBriques == 0){
             SceneManager.LoadScene("Victory");
+        }
+        if(collision.gameObject.tag == "Raquette"){
+            // let's check where it collides:
+            // print("First point that collided: " + collision.contacts[0].point.x);
+            // now we can do something:
+            if(collision.contacts[0].point.x <= -1.4 || collision.contacts[0].point.x <= 1.4){
+                //print("kick the ball !!!");
+                // we are going to put inside variable for the moment....
+                transform.Translate(Time.deltaTime* deplacement * sidePower);
+            }
         }
     }
 }
