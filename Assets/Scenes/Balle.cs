d732842a24a93468251df7d80e3ad62d6be9a177
Patricio <jpdibacco@gmail.com>
2020-11-05 16:35:11 +0100

    added Victory Scene

--- Assets/Scenes/Balle.cs
+++ Assets/Scenes/Balle.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Balle : MonoBehaviour{
     public Vector3 deplacement;
+    public int nbrBriques;
     void FixedUpdate(){
         transform.Translate(Time.deltaTime*deplacement);
     }
@@ -11,6 +12,10 @@ public class Balle : MonoBehaviour{
         deplacement = Vector3.Reflect(deplacement,collision.contacts[0].normal);
         if(collision.gameObject.tag == "Brique"){
             Destroy(collision.gameObject);
+            nbrBriques--;
+        }
+        if (nbrBriques == 0){
+            SceneManager.LoadScene("Victory");
         }
     }
 }
