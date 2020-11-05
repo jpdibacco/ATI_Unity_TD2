862875716236d52816369d25d0fce9ac906ca5c7
Patricio <jpdibacco@gmail.com>
2020-11-05 17:55:22 +0100

    Force à l'apparittion

---
+++ Assets/Scenes/DebrisBrique.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisBrique : MonoBehaviour{
+    public float force = 10f;
+    void Start(){
+        Vector3 motion = new Vector3(Random.Range(-1.0f, 1.0f), 1, 0);
+        GetComponent<Rigidbody>().AddForce(motion*force);
+    }
+
+}
