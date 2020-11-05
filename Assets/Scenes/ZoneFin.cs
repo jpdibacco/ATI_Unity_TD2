744d7ff93cb00bf9a0235159c5d2b70a0cc2f880
Patricio <jpdibacco@gmail.com>
2020-11-05 16:17:47 +0100

    added Title screen

--- Assets/Scenes/ZoneFin.cs
+++ Assets/Scenes/ZoneFin.cs
@@ -1,18 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ZoneFin : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+using UnityEngine.SceneManagement;
+public class ZoneFin : MonoBehaviour{
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Balle"){
+            //SceneManager.LoadScene(0);
+        }
     }
 }
